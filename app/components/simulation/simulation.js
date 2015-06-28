4ff318caa4b7b9c3bea2638232077d4d261f1e61
Marc Pare <marcpare0@gmail.com>
2015-06-27 22:51:26 -0700

    Wire up raw Wagner simulation to canvas
    Not positioned properly, but does run.

--- app/components/simulation/simulation.js
+++ app/components/simulation/simulation.js
@@ -1,5 +1,12 @@
 let AmpersandView = require('ampersand-view');
+let StamWagnerSimulation = require('app/lib/stam-wagner-simulation');
 
 module.exports = AmpersandView.extend({
-  template: require('app/components/simulation/simulation.jade')
+  template: require('app/components/simulation/simulation.jade'),
+  
+  render () {
+    this.renderWithTemplate(this);
+    StamWagnerSimulation(this.query('#c'));
+    return this;
+  }
 });
