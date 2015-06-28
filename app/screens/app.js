0bbd2afa864a016d51e703ebe1eaf625196f4705
Marc Pare <marcpare0@gmail.com>
2015-06-27 22:25:25 -0700

    Render the sidebar as a subview of app screen

--- app/screens/app.js
+++ app/screens/app.js
@@ -1,5 +1,13 @@
 let AmpersandView = require('ampersand-view');
+let SidebarView = require('app/components/sidebar/sidebar');
 
 module.exports = AmpersandView.extend({
-  template: require('app/templates/app.jade')
+  template: require('app/templates/app.jade'),
+  
+  subviews: {
+    sidebar: {
+      container: '#sidebar-container',
+      constructor: SidebarView
+    }
+  }
 });
