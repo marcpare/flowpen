fb6c4ce73fa4e4afb2b7ed1d1babfa7f9f26a944
Marc Pare <marcpare0@gmail.com>
2015-06-27 22:30:00 -0700

    Render a placeholder simulation container

---
+++ app/components/simulation/simulation.js
@@ -0,0 +1,5 @@
+let AmpersandView = require('ampersand-view');
+
+module.exports = AmpersandView.extend({
+  template: require('app/components/simulation/simulation.jade')
+});
--- app/screens/app.js
+++ app/screens/app.js
@@ -1,5 +1,6 @@
 let AmpersandView = require('ampersand-view');
 let SidebarView = require('app/components/sidebar/sidebar');
+let SimulationView = require('app/components/simulation/simulation');
 
 module.exports = AmpersandView.extend({
   template: require('app/templates/app.jade'),
@@ -8,6 +9,11 @@ module.exports = AmpersandView.extend({
     sidebar: {
       container: '#sidebar-container',
       constructor: SidebarView
+    },
+    
+    simulation: {
+      container: '#simulation-container',
+      constructor: SimulationView
     }
   }
 });
