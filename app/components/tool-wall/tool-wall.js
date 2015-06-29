a1dc03b96bba74a50c949ee92f599d32a0025123
Marc Pare <marcpare0@gmail.com>
2015-06-28 17:06:36 -0700

    Set caption on prototype, not in `initialize`

--- app/components/tool-wall/tool-wall.js
+++ app/components/tool-wall/tool-wall.js
@@ -1,7 +1,7 @@
 let ToolView = require('app/components/tool/tool');
 
 module.exports = ToolView.extend({  
-  initialize (options) {
-    this.caption = 'Wall';
-  }
+  
+  caption: 'Wall'
+  
 });
--- app/components/tool/tool.js
+++ app/components/tool/tool.js
@@ -3,7 +3,5 @@ let AmpersandView = require('ampersand-view');
 module.exports = AmpersandView.extend({
   template: require('app/components/tool/tool.jade'),
   
-  initialize (options) {
-    this.caption = '<Missing Caption>';
-  }
+  caption: '<Missing Caption>'
 });
