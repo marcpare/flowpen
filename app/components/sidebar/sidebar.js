d4d6f3895fe896960e79bddf0fadb01d035dc947
Marc Pare <marcpare0@gmail.com>
2015-06-28 17:02:48 -0700

    Split tool buttons into views

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -1,5 +1,19 @@
 let AmpersandView = require('ampersand-view');
+let WallToolView = require('app/components/tool-wall/tool-wall');
+let InletToolView = require('app/components/tool-inlet/tool-inlet');
 
 module.exports = AmpersandView.extend({
-  template: require('app/components/sidebar/sidebar.jade')
+  template: require('app/components/sidebar/sidebar.jade'),
+  
+  subviews: {
+    wallTool: {
+      container: '#wall-tool-container',
+      constructor: WallToolView
+    },
+    
+    inletTool: {
+      container: '#inlet-tool-container',
+      constructor: InletToolView
+    }
+  }
 });
---
+++ app/components/tool-inlet/tool-inlet.js
@@ -0,0 +1,9 @@
+let AmpersandView = require('ampersand-view');
+
+module.exports = AmpersandView.extend({
+  template: require('app/components/tool-wall/tool-wall.jade'),
+  
+  initialize (options) {
+    this.caption = 'Inlet';
+  }
+});
