6faa90a19d2a3483ca91f9be0251c1b3af104eb3
Marc Pare <marcpare0@gmail.com>
2015-07-16 20:13:10 -0700

    Simpler sharing/contact

--- app/components/sidebar/sidebar.js
+++ app/components/sidebar/sidebar.js
@@ -2,14 +2,10 @@ let AmpersandView = require('ampersand-view');
 let WallToolView = require('app/components/tool-wall/tool-wall');
 let InletToolView = require('app/components/tool-inlet/tool-inlet');
 let ClearControlView = require('app/components/control-clear/clear-control');
-let ModalView = require('app/components/modal/modal');
 
 module.exports = AmpersandView.extend({
   template: require('app/components/sidebar/sidebar.jade'),
   
-  events: {
-    'click [data-hook=share]': 'share'
-  },
   
   subviews: {
     wallTool: {
@@ -28,10 +24,4 @@ module.exports = AmpersandView.extend({
     }
   },
   
-  share: function () {
-       
-    let modal = new ModalView();
-    modal.openIn('body');
-    
-  }
 });
