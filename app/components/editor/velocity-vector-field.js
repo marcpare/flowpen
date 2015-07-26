177e027eaf3acce5eac4f64968d711c62941a94d
Marc Pare <marcpare0@gmail.com>
2015-07-25 21:31:52 -0700

    Cool demo of many tiny velocity vectors

--- app/components/editor/velocity-vector-field.js
+++ app/components/editor/velocity-vector-field.js
@@ -13,14 +13,20 @@ module.exports = View.extend({
     // meshgrid(width, height).each(x, y => {
     //
     // });
+    //
+    // Another idea: Cartesian Join
+    //
+    // _.join(_.range(width), _.range(height)).each(x, y => {
+    //
+    // })
       
-    for (let x = 0; x < Simulation.width; x+=10) {
-      for (let y = 0; y < Simulation.height; y+=10) {
+    for (let x = 1; x < Simulation.width; x+=3) {
+      for (let y = 1; y < Simulation.height; y+=3) {
         this.svg
           .line(x, y, x+1, y+1)
           .attr({
             stroke: '#bada55',
-            strokeWidth: 0.5,
+            strokeWidth: 0.1,
             strokeLinecap: 'round'
           });
       
