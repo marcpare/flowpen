eb6d3f65b4c17395e75f2ce9f22feb2012e7eeef
Marc Pare <marcpare0@gmail.com>
2015-07-25 21:26:27 -0700

    Build a mesh of little lines

--- app/components/editor/velocity-vector-field.js
+++ app/components/editor/velocity-vector-field.js
@@ -1,18 +1,33 @@
 let View = require('ampersand-view');
+let Simulation = require('app/models/simulation');
  
 module.exports = View.extend({
-
-  create (svg) {    
-    this.svg = svg
-      .line(0, 0, 10, 10)
-      .attr({
-        stroke: '#bada55',
-        strokeWidth: 2,
-        strokeLinecap: 'round'
-      });
+  
+  create (svg) {
+    
+    this.svg = svg.group();
+          
+    // build a group of many tiny lines
+        
+    // A dream sort of syntax for this:
+    // meshgrid(width, height).each(x, y => {
+    //
+    // });
+      
+    for (let x = 0; x < Simulation.width; x+=10) {
+      for (let y = 0; y < Simulation.height; y+=10) {
+        this.svg
+          .line(x, y, x+1, y+1)
+          .attr({
+            stroke: '#bada55',
+            strokeWidth: 0.5,
+            strokeLinecap: 'round'
+          });
+      
+      }
+    }
     
     this.el = this.svg.node;
-    // this.listenTo(this.model, 'change', this.update.bind(this));
   },  
    
 });
