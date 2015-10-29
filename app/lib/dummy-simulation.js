cd7b9e573ad4d1d58dde975e2ec34aa382a5612a
Marc Pare <marcpare0@gmail.com>
2015-10-29 10:03:31 -0700

    Dummy simulation can draw

--- app/lib/dummy-simulation.js
+++ app/lib/dummy-simulation.js
@@ -1,1 +1,16 @@
-module.exports = require('app/lib/simulation-base')({});
+module.exports = require('app/lib/simulation-base')({
+  draw (width, height, d) {
+    let di, pi, ui, x, y;
+    for(y = 0; y < height; y++) {
+      for(x = 0; x < width; x++) {
+        pi = (y*width+x);
+        ui = pi*2;
+        di = pi*4;
+
+        d[di+0] = 123;
+        d[di+1] = 123;
+        d[di+2] = 321;
+      }
+    }
+  }
+});
--- app/lib/simulation-base.js
+++ app/lib/simulation-base.js
@@ -16,13 +16,50 @@
 //
 
 module.exports = function (options) {
+
+  let initialize = options.initialize || () => {};
+  let simulate = options.simulation || () => {};
+  let draw = options.draw || () => {};
+  let ctx = false;
+  let imageData = false;
+  let WIDTH = false;
+  let HEIGHT = false;
+
+  let requestAnimationFrame = (window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame    ||
+    window.oRequestAnimationFrame      ||
+    window.msRequestAnimationFrame     ||
+    function (callback) {
+      window.setTimeout(callback, 1000 / 60);
+    });
+  let running = true;
+
+  function animate () {
+    if (!running) return;
+    simulate();
+    if (ctx && imageData) {
+      draw(WIDTH, HEIGHT, imageData.data);
+      ctx.putImageData(imageData, 0, 0);
+    }
+    requestAnimationFrame(animate);
+  }
+  animate();
+
   return function (canvas, options) {
+
+    WIDTH = options.columns || 128;
+    HEIGHT = options.rows || 128;
+
+    ctx = canvas.getContext('2d');
+    ctx.fillRect(0, 0, WIDTH, HEIGHT);
+    imageData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
 
     function resize () {}
     function velocityAt () {}
     function addWall () {}
     function addInlet () {}
-    function stop () {}
+    function stop () { running = false; }
 
     return {
       resize,
