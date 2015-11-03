f32e687e26d7ee2ef05dc70b4711663c23e5dee0
Marc Pare <marcpare0@gmail.com>
2015-11-03 10:30:20 -0800

    Implement indexing function I and velocityBoundary

--- app/lib/simulation-base.js
+++ app/lib/simulation-base.js
@@ -27,6 +27,8 @@ let requestAnimationFrame = (window.requestAnimationFrame ||
     window.setTimeout(callback, 1000 / 60);
   });
 
+let I = () => {};
+
 class SimulationBase {
   constructor () {
     this.running = false;
@@ -49,28 +51,46 @@ class SimulationBase {
     this.inletVelocityField = new Float32Array(N*2);
     fill(this.inletVelocityField, 0.0);
 
-    this.velocityField0 = new Float32Array(N*2);
-    this.velocityField1 = new Float32Array(N*2);
-    this.pressureField0 = new Float32Array(N);
-    this.pressureField1 = new Float32Array(N);
-    this.divergenceField = new Float32Array(N);
+    this.u0x = new Float32Array(N);
+    this.u0y = new Float32Array(N);
+    this.u1x = new Float32Array(N);
+    this.u1y = new Float32Array(N);
+    this.p0 = new Float32Array(N);
+    this.p1 = new Float32Array(N);
+    this.div = new Float32Array(N);
+
     this.step = 4.0;
 
-    fill(this.pressureField0, 0.0);
-    fill(this.pressureField1, 0.0);
-    fill(this.velocityField0, 0.0);
-    fill(this.velocityField1, 0.0);
+    fill(this.u0x, 0.0);
+    fill(this.u0y, 0.0);
+    fill(this.u1x, 0.0);
+    fill(this.u1y, 0.0);
+    fill(this.p0, 0.0);
+    fill(this.p1, 0.0);
+    fill(this.div, 0.0);
 
-    // TODO: do these get replace with macros or functions, or..?
-    // u0x = sampler(velocityField0, WIDTH, HEIGHT, 2, 0),
-    // u0y = sampler(velocityField0, WIDTH, HEIGHT, 2, 1),
-    // u1x = sampler(velocityField1, WIDTH, HEIGHT, 2, 0),
-    // u1y = sampler(velocityField1, WIDTH, HEIGHT, 2, 1),
-    // p0 = sampler(pressureField0, WIDTH, HEIGHT, 1, 0),
-    // p1 = sampler(pressureField1, WIDTH, HEIGHT, 1, 0),
-    // div = sampler(divergenceField, WIDTH, HEIGHT, 1, 0),
-    // velocityboundary(u0x, u0y);
+    // Indexing function for the flattened 2D arrays
+    I = (x, y) => { return y*N + x; };
+  }
 
+  // Set the velocity to zero along the edges of the simulation
+  // ux: x velocity
+  // uy: y velocity
+  velocityboundary (ux, uy) {
+    let i;
+    for (i = 0; i < this.width; i++) {
+      ux[I(i, 0)] = 0;
+      uy[I(i, 0)] = 0;
+      ux[I(i, this.height-1)] = 0;
+      uy[I(i, this.height-1)] = 0;
+
+    }
+    for (i = 0; i < this.height; i++) {
+      ux[I(0, i)] = 0;
+      uy[I(0, i)] = 0;
+      ux[I(this.width-1, i)] = 0;
+      uy[I(this.width-1, i)] = 0;
+    }
   }
 
   start () {
